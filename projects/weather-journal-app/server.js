3ad6b640347b58ae185f588ea421436543a270db
Mamdouh-Mansour <mamdouh.mansour@meccasoft.com>
2023-02-02 02:53:46 +0200

    Finalize Project

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -1,19 +1,57 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const projectData = [];
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+const port = 3000;
+const server = app.listen(port, listening);
 
+function listening(){
+    console.log("server running");
+    console.log(`running on localhost: ${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
+}
 // Setup Server
+//Get weather data route
+app.get('/getweatherdata', getWeatherData);
+
+function getWeatherData(req, res){
+    console.log("getWeatherData requested = > " + projectData)
+    res.send(projectData.slice(-1));
+}
+
+
+// post weather data route
+app.post('/addweatherdata', addWeatherData);
+
+function addWeatherData(req, res){
+    console.log("getWeatherData requested = > " + projectData)
+    console.log("Weather Data Are ==> " + req.body);
+    
+    newWeatherEntry = {
+        temperature: req.body.temperature,
+        date: req.body.date,
+        feelings: req.body.feelings
+    }
+
+    projectData.push(newWeatherEntry);
+    res.send(projectData);
+    console.log("Weather Data Are ==> " + projectData[0].temperature +  projectData[0].date +  projectData[0].feelings);
+}
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,5 +1,69 @@
 /* Global Variables */
+const API_KEY = "12069046be7b3bec6cfefe3a43889813";
+const getWeatherDataEndPoint = "/getweatherdata";
+const postWeatherDataEndPoint = "/addweatherdata";
+const baseUrl = "https://api.openweathermap.org/data/2.5/weather?";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+//event 
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e) {
+    const feelings = document.getElementById('feelings').value;
+    const zipCode = document.getElementById('zip').value;
+
+    getOpenMapWetherDataByZipCode(baseUrl,zipCode,API_KEY)
+        .then((data) =>
+            postWeatherData(postWeatherDataEndPoint, { "temperature": data.main.temp, "date": newDate, "feelings": feelings }))
+        .then(() => getWetherJournalData());
+}
+
+//Get weather data from openMapWehater APP
+const getOpenMapWetherDataByZipCode = async (baseUrl, zipCode, apiKey) => {
+    const openMapWehaterUri = `${baseUrl}zip=${zipCode},us&appid=${apiKey}&units=metric`;
+
+    const res = await fetch(openMapWehaterUri);
+    try {
+        const data = await res.json();
+        return data;    
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+
+//Add data to weather journal app
+
+const postWeatherData = async (url = '', data = {}) => {
+    const res = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    try {
+        const postedData = await res.json();
+        return postedData;
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+
+//Get weather request
+
+const getWetherJournalData = async () => {
+    const res = await fetch(getWeatherDataEndPoint);
+    try {
+        const data = await res.json();
+        document.getElementById('temp').innerHTML = "Current Temperatuer is: " + data[0].temperature;
+        document.getElementById('date').innerHTML = "Date: " + data[0].date;
+        document.getElementById('content').innerHTML = "User Feeling: " + data[0].feelings;
+    } catch (error) {
+        console.log("error", error);
+    }
+}
